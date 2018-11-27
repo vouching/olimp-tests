71d61fc38359275f7e6107f1775c8e0cab023084
vouching <v_ned@mail.ru>
2018-11-28 00:09:51 +0300

    umeet menyati programmy

--- test2.cpp
+++ test2.cpp
@@ -5,6 +5,10 @@
 #include <vector>
 #include <iomanip>
 using namespace std;
+int amount_programmes;
+int duration_of_pressing;
+int number_of_pressings;
+int position_of_favourite;
 void pr(vector <int> a)
 {
     for(unsigned int i=0;i<a.size();i++)
@@ -13,19 +17,28 @@ void pr(vector <int> a)
     }
     cout<<endl;
 }
+void swapp(vector <int>& a,int duration)
+{
+    
+    duration=duration%amount_programmes-1;
+    int f =a[duration];
+    a[duration]=a[a.size()-1];
+    a[a.size()-1]=f;
+}
 int main()
 {
-    int amount_programmes;
-    int duration_of_pressing;
-    int number_of_pressings;
-    int position_of_favourite;
+    
     cin>>amount_programmes>>position_of_favourite>>number_of_pressings;
     vector <int> open_prog;
     int i=0;
-    for(i=0;i<amount_programmes;i++)
+    for(i=1;i<amount_programmes;i++)
     {
         open_prog.push_back(i);
-        cout<<open_prog[i];
     }  
+    open_prog.push_back(0);
+    position_of_favourite--;
+
+    pr(open_prog);
+    swapp(open_prog,3);
     pr(open_prog);
 }
