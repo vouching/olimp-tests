e201557b2c0530d793068369388af2e136084f95
vouching <v_ned@mail.ru>
2018-11-27 23:34:50 +0300

    1 этап

---
+++ test2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int amount_programmes;
+    int duration_of_pressing;
+    int number_of_pressings;
+    int position_of_favourite;
+     
+}
