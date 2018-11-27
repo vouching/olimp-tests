ea4bb8c2676e59f0a45a93b67e69d9c4cda5fe8e
vouching <v_ned@mail.ru>
2018-11-27 23:41:07 +0300

    vvod peremenny

--- test2.cpp
+++ test2.cpp
@@ -11,6 +11,7 @@ int main()
     int duration_of_pressing;
     int number_of_pressings;
     int position_of_favourite;
+    cin>>amount_programmes>>position_of_favourite>>number_of_pressings;
     vector <int> open_prog;
     int i=0;
     for(i=0;i<amount_programmes;i++);
